25fcbb973c6b51928875187ce53337806b5e43d5
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-15 12:22:11 -0600

    reorganized libs and added RDG

--- src/Libs.hx
+++ src/Libs.hx
@@ -16,60 +16,37 @@ typedef LibConf = {
 
 class Libs
 {
-
+  static var libraries = [
+              {name:"thx.color"},
+              {name:"thx.core"},
+              {name:"thx.culture"},
+              {name:"thx.format"},
+              {name:"thx.promise"},
+              {name:"thx.semver"},
+              {name:"thx.stream"},
+              //{name:"thx.text"},
+              {name:"thx.unit"},
+              {name:"rdg"},
+              {name:"utest"},
+              {name:"actuate"},
+              {name:"format" },
+              {name:"nape"},
+              {name:"hscript"},
+              {name:"haxeui"},
+              {name:"yaml"},
+            ];
   static var available : Map<String, Array<LibConf>> = [
-    "JS" => [
-      //{name:"nme", args : ["--remap","flash:browser"], head : ["<link rel='stylesheet' href='../swf.css' type='text/css'/>"], body:["<div id='haxe:jeash'></div>"]},
-      {name:"thx.color"},
-      {name:"thx.core"},
-      {name:"thx.culture"},
-      {name:"thx.format"},
-      {name:"thx.promise"},
-      {name:"thx.semver"},
-      {name:"thx.stream"},
-      //{name:"thx.text"},
-      {name:"thx.unit"},
-      {name:"sui"},
-      {name:"minicanvas"},
-      {name:"utest"},
-      {name:"actuate"},
-      //{name:"selecthx"},
-      //{name:"modernizr"},
-      //{name:"browserhx"},
-      {name:"format" },
-      {name:"nape"},
-      {name:"hscript"},
-      {name:"haxeui"},
-      {name:"yaml"},
-      //{name:"three.js", head: ["<script src='../../../lib/js/stats-min.js'></script>", "<script src='../../../lib/js/three-min.js'></script>"]}
-    ],
-    "SWF" => new Array<LibConf>().concat([
-      {name:"thx.color"},
-      {name:"thx.core"},
-      {name:"thx.culture"},
-      {name:"thx.format"},
-      {name:"thx.promise"},
-      {name:"thx.semver"},
-      {name:"thx.stream"},
-      //{name:"thx.text"},
-      {name:"thx.unit"},
-      {name:"sui"},
-      {name:"minicanvas"},
-      {name:"utest"},
-      {name:"actuate" , args : []},
-      {name:"format"},
-      {name:"nape"},
-      {name:"hscript"},
-      {name:"haxeui"},
-      {name:"yaml"},
-      {name:"away3d", swf:{src:"away3d4.swf"}, help:"http://away3d.com/livedocs/away3d/4.0/"},
-      //{name:"starling" },
-    ])
+    "JS"  => libraries.concat([
+        {name:"sui"},
+        {name:"minicanvas"},
+      ]),
+    "SWF" => libraries
   ];
 
+  static var defaultSelected = ["thx.color", "thx.core", "thx.culture", "thx.format", "thx.promise", "thx.semver", "thx.stream", "thx.unit"];
   static var defaultChecked : Map < String, Array<String> > = [
-    "JS" => ["thx.color", "thx.core", "thx.culture", "thx.format", "thx.promise", "thx.semver", "thx.stream", "thx.unit", "sui", "minicanvas"],
-    "SWF" => ["thx.color", "thx.core", "thx.culture", "thx.format", "thx.promise", "thx.semver", "thx.stream", "thx.unit", "sui", "minicanvas"]
+    "JS" => defaultSelected.concat(["minicanvas", "sui"]),
+    "SWF" => defaultSelected
   ]; // array of lib names
 
   static public function getLibsConfig(?target:Target, ?targetName:String):Array<LibConf>
